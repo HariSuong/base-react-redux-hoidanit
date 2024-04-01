2f13d3651cb76d28c69489e7a0f3ae1636f9948b
Hari Suong <suongmec39@gmail.com>
2024-04-01 19:09:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import Header from './components/Header/Header'
+import { Outlet, Link } from 'react-router-dom'
 
 const App = () => {
-  return <div className='app-container'>Hihii with Hari</div>
+  return (
+    <div className='app-container'>
+      <Header />
+      <div>Test</div>
+      <div>
+        <button>
+          <Link to={'/users'}>Go to user test</Link>
+        </button>
+        <button>
+          <Link to={'/admin'}>Go to admin test</Link>{' '}
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
